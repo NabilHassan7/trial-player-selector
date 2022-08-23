684d7fbe75868fb960aadb2b98c1e17ffe59db3f
NabilHassan7 <fakrulibnehassan@gmail.com>
2022-08-23 20:57:30 +0600

    fixed minor issues

--- js/utilities.js
+++ js/utilities.js
@@ -70,13 +70,13 @@ function inputValueValidator(elementId, elementValue){
     else if(elementValue < 0){
         const emptyString = ' ';
         setElementValueNumberById(elementId,emptyString);
-        alert('Budget cannot be a negative number.');
+        alert('Input cannot be a negative number.');
         return false;
     }
     else if(elementValue === ' '){
         const emptyString = ' ';
         setElementValueNumberById(elementId,emptyString);
-        alert('Please enter a number in the cost per player field');
+        alert('Please enter a number in the input field');
         return false;
     }
     else{
